55517dfa1d44019bd9d0d6de9ce81c92ea222bbf
Vladislav1992 <kuryanov.vlad@yandex.ru>
2024-01-18 18:26:47 +0300

    3. Доменная модель проекта "АвтоМаг" [#504897]

--- db/scripts/002_ddl_create_posts_table.sql
+++ db/scripts/002_ddl_create_posts_table.sql
@@ -2,5 +2,5 @@ create table auto_post (
     id serial primary key,
     description text,
     created timestamp,
-    auto_user_id references auto_user(id)
+    auto_user_id int references auto_user(id)
 );
